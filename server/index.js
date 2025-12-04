7c7924090d97b7f11aaa1ad3a25bf2982c63055f
anu-ushka <anushkatyagi.2013@gmail.com>
2025-12-04 22:59:58 +0530

    edit9

--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://bee-better-u8io.vercel.app/"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Database Connection
