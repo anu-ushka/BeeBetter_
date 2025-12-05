42fc31dea31a015733b0d1fcf9d5162de5096fb1
anu-ushka <anushkatyagi.2013@gmail.com>
2025-12-05 13:29:42 +0530

    edit 21

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,16 +18,12 @@ const Home = () => {
                 <span className="font-bold text-xl tracking-tight text-gray-900">BeeBetter</span>
             </nav>
 
-            {/* Main Content */}
             <main className="relative z-10 flex-1 flex flex-col items-center justify-center text-center px-4">
 
-                {/* Animated Bee Logo */}
                 <div className="mb-8 relative">
-                    {/* White background container for the bee */}
                     <div className="bg-white p-6 rounded-full shadow-xl animate-fly relative z-10">
                         <img src="/bee-logo.png" alt="BeeBetter Logo" className="w-32 h-32 object-contain" />
                     </div>
-                    {/* Decorative blob behind */}
                     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-48 h-48 bg-primary opacity-20 rounded-full blur-3xl -z-10"></div>
                 </div>
 
